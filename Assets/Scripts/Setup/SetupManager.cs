794b3cc5afa93d14904e71edf48174f6a950afde
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2024-01-30 20:57:08 +0200

    finished beautiful side of setup UI

---
+++ Assets/Scripts/Setup/SetupManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupManager : MonoBehaviour
+{
+    //Defined constants (sort of config for default enviroment)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
