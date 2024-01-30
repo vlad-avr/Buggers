4baab9746c7189613d58eaf88d1a76bf77dab9f8
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2024-01-30 21:16:29 +0200

    I'm done for today. Burnt out (no significant progress)

--- Assets/Scripts/Setup/SetupManager.cs
+++ Assets/Scripts/Setup/SetupManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SetupManager : MonoBehaviour
 {
     //Defined constants (sort of config for default enviroment)
-
+    private const string config_path = "Assets/Resources/Config/enironment_config.txt";
     // Start is called before the first frame update
     void Start()
     {
