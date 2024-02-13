da504eede7b8d92afb70590665554aa8bae2d191
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2024-02-13 14:48:43 +0200

    started work on supervised learning part

---
+++ Assets/Scripts/Controller/ManualController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualController : MonoBehaviour
+{
+
+
+    public float speed;
+    public float angular_drag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
